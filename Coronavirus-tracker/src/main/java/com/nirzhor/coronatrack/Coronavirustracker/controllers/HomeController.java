bf4589bf6780ac43aea250f4812084454e2ff22e
Nirzhor <rabbi.cse1.bu@gmail.com>
2020-09-01 17:53:56 +0600

    Comma added in display

--- Coronavirus-tracker/src/main/java/com/nirzhor/coronatrack/Coronavirustracker/controllers/HomeController.java
+++ Coronavirus-tracker/src/main/java/com/nirzhor/coronatrack/Coronavirustracker/controllers/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 @Controller
@@ -20,11 +21,16 @@ public class HomeController {
     public String home(Model model) {
 
         List<LocationStats> allStatesConfirmed = coronaVirusDataService.getAllStatsConfirmed();
-       int totalReportedCases =  allStatesConfirmed.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalReportedCases =  allStatesConfirmed.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
         int totalNewCases =  allStatesConfirmed.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+
+        DecimalFormat displayFormatter = new DecimalFormat("###,###");
+        String totalReportedCasesDisplay = displayFormatter.format(totalReportedCases);
+        String totalNewCasesDisplay = displayFormatter.format(totalNewCases);
+
         model.addAttribute("locationStats",allStatesConfirmed);
-        model.addAttribute("totalReportedCases",totalReportedCases);
-        model.addAttribute("totalNewCases",totalNewCases);
+        model.addAttribute("totalReportedCases",totalReportedCasesDisplay);
+        model.addAttribute("totalNewCases",totalNewCasesDisplay);
 
 
 
